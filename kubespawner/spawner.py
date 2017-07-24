5928eac7666ece95233572c622aa8f9e5abe326a
Clement Nero <clement.nero@fr.pwc.com>
2017-07-24 19:22:04 +0200

    Dynamic CPU an RAM configuration from user form options.

--- kubespawner/spawner.py
+++ kubespawner/spawner.py
@@ -320,17 +320,19 @@ class KubeSpawner(Spawner):
             'mountPath': '/var/run/secrets/kubernetes.io/serviceaccount',
             'readOnly': True
         }]
-        return make_pod_spec(
+        spec = make_pod_spec(
             self.pod_name,
             self.singleuser_image_spec,
             self.get_env(),
             self._expand_all(self.volumes) + hack_volumes,
             self._expand_all(self.volume_mounts) + hack_volume_mounts,
-            self.cpu_limit,
+            self.user_options['cpu'],#self.cpu_limit,
             self.cpu_guarantee,
-            self.mem_limit,
+            self.user_options['memory'],#self.mem_limit,
             self.mem_guarantee,
         )
+        self.log.info("About to deploy the following kube spec: " + str(spec))
+        return spec
 
     def get_pvc_manifest(self):
         """
